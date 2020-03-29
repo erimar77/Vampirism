9be820961890c45fcae2f7994255c4e5f1406a97
Paul Becker <paube@mail.upb.de>
2020-03-29 18:26:32 +0200

    remove required items

--- src/main/java/de/teamlapen/vampirism/proxy/CommonProxy.java
+++ src/main/java/de/teamlapen/vampirism/proxy/CommonProxy.java
@@ -1,8 +1,11 @@
 package de.teamlapen.vampirism.proxy;
 
 
+import de.teamlapen.vampirism.entity.factions.FactionPlayerHandler;
+import de.teamlapen.vampirism.network.TaskFinishedPacket;
 import de.teamlapen.vampirism.player.skills.SkillTree;
 import de.teamlapen.vampirism.player.skills.SkillTreeManager;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraftforge.fml.event.lifecycle.ModLifecycleEvent;
 
 /**
@@ -20,5 +23,13 @@ public abstract class CommonProxy implements IProxy {
 
     @Override
     public void onInitStep(Step step, ModLifecycleEvent event) {
+    }
+
+    @Override
+    public void handleTaskFinishedPacket(TaskFinishedPacket msg, PlayerEntity playerEntity) {
+        FactionPlayerHandler.getOpt(playerEntity).ifPresent(factionPlayerHandler -> factionPlayerHandler.getCurrentFactionPlayer().ifPresent(sd -> {
+            sd.getTaskManager().addCompletedTask(msg.task);
+            sd.getTaskManager().removeRequirements(msg.task);
+        }));
     }
 }
